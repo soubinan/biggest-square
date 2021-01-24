08abd9d9cc766a4309bd9fc340376d972590e746
Soubinan <soubinan@gmail.com>
2021-01-24 19:16:54 +0100

    update Map tests

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from map_gen import map_gen as gen
 
+import os
 
 def find_square():
     print('Find square')
 
 
-if __name__ == "__main__":
+class Map:
+  def __init__(self, map_file_path=''):
+    print(os.getcwd(), os.path.abspath(map_file_path))
+    try:
+        with open(map_file_path) as f:
+            [print(line) for line in f.readlines()]
+    except Exception as e:
+        print('Error:', e)
+
+
+if __name__ == '__main__':
     find_square()
--- tests/test_map.py
+++ tests/test_map.py
@@ -10,30 +10,29 @@ class TestMapValidation(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.map_01 = Map('./samples/bad-1')
-        cls.map_02 = Map('./samples/bad-2')
-        cls.map_03 = Map('./samples/bad-3')
-        cls.map_04 = Map('./samples/bad-4')
-        cls.map_05 = Map('./samples/bad-5')
-        cls.map_06 = Map('./samples/bad-6')
+        cls.map_01 = Map('./tests/samples/bad-1')
+        cls.map_02 = Map('./tests/samples/bad-2')
+        cls.map_03 = Map('./tests/samples/bad-3')
+        cls.map_04 = Map('./tests/samples/bad-4')
+        cls.map_05 = Map('./tests/samples/bad-5')
+        cls.map_06 = Map('./tests/samples/bad-6')
 
-        cls.map_07 = Map('./samples/easy-1')
-        cls.map_08 = Map('./samples/easy-2')
-        cls.map_09 = Map('./samples/easy-3')
+        cls.map_07 = Map('./tests/samples/easy-1')
+        cls.map_08 = Map('./tests/samples/easy-2')
+        cls.map_09 = Map('./tests/samples/easy-3')
 
-        cls.map_10 = Map('./samples/normal-1')
-        cls.map_11 = Map('./samples/normal-2')
-        cls.map_12 = Map('./samples/normal-3')
+        cls.map_10 = Map('./tests/samples/normal-1')
+        cls.map_11 = Map('./tests/samples/normal-2')
+        cls.map_12 = Map('./tests/samples/normal-3')
 
-        cls.map_13 = Map('./samples/hard-1')
-        cls.map_14 = Map('./samples/hard-2')
-        cls.map_15 = Map('./samples/hard-3')
-        cls.map_16 = Map('./samples/hard-4')
-        cls.map_17 = Map('./samples/hard-5')
-        cls.map_18 = Map('./samples/hard-6')
+        cls.map_13 = Map('./tests/samples/hard-1')
+        cls.map_14 = Map('./tests/samples/hard-2')
+        cls.map_15 = Map('./tests/samples/hard-3')
+        cls.map_16 = Map('./tests/samples/hard-4')
+        cls.map_17 = Map('./tests/samples/hard-5')
+        cls.map_18 = Map('./tests/samples/hard-6')
 
-        cls.map_19 = Map('./samples/exercise-test-input')
-        cls.map_20 = Map('./samples/exercise-test-output')
+        cls.map_19 = Map('./tests/samples/exercise-test-input')
 
 
     def test_validate_map(self):
@@ -60,33 +59,30 @@ class TestMapValidation(unittest.TestCase):
         self.assertEqual(self.map_18.validate_map, True)
 
         self.assertEqual(self.map_19.validate_map, True)
-        self.assertEqual(self.map_20.validate_map, True)
 
 
     def test_get_maximum_possible_size(self):
-        self.assertEqual(self.map_01.get_maximum_possible_size, True)
-        self.assertEqual(self.map_02.get_maximum_possible_size, True)
-        self.assertEqual(self.map_03.get_maximum_possible_size, True)
-        self.assertEqual(self.map_04.get_maximum_possible_size, True)
-        self.assertEqual(self.map_05.get_maximum_possible_size, True)
-        self.assertEqual(self.map_06.get_maximum_possible_size, True)
+        self.assertEqual(self.map_01.get_maximum_possible_size, 0)
+        self.assertEqual(self.map_02.get_maximum_possible_size, 0)
+        self.assertEqual(self.map_03.get_maximum_possible_size, 0)
+        self.assertEqual(self.map_04.get_maximum_possible_size, 0)
+        self.assertEqual(self.map_05.get_maximum_possible_size, 0)
+        self.assertEqual(self.map_06.get_maximum_possible_size, 0)
 
-        self.assertEqual(self.map_07.get_maximum_possible_size, True)
-        self.assertEqual(self.map_08.get_maximum_possible_size, True)
-        self.assertEqual(self.map_09.get_maximum_possible_size, True)
+        self.assertEqual(self.map_07.get_maximum_possible_size, 8)
+        self.assertEqual(self.map_08.get_maximum_possible_size, 2)
+        self.assertEqual(self.map_09.get_maximum_possible_size, 1)
 
-        self.assertEqual(self.map_10.get_maximum_possible_size, True)
-        self.assertEqual(self.map_11.get_maximum_possible_size, True)
-        self.assertEqual(self.map_12.get_maximum_possible_size, True)
+        self.assertEqual(self.map_10.get_maximum_possible_size, 10)
+        self.assertEqual(self.map_11.get_maximum_possible_size, 10)
+        self.assertEqual(self.map_12.get_maximum_possible_size, 10)
 
-        self.assertEqual(self.map_13.get_maximum_possible_size, True)
-        self.assertEqual(self.map_14.get_maximum_possible_size, True)
-        self.assertEqual(self.map_15.get_maximum_possible_size, True)
-        self.assertEqual(self.map_16.get_maximum_possible_size, True)
-        self.assertEqual(self.map_17.get_maximum_possible_size, True)
-        self.assertEqual(self.map_18.get_maximum_possible_size, True)
+        self.assertEqual(self.map_13.get_maximum_possible_size, 9)
+        self.assertEqual(self.map_14.get_maximum_possible_size, 8)
+        self.assertEqual(self.map_15.get_maximum_possible_size, 9)
+        self.assertEqual(self.map_16.get_maximum_possible_size, 10)
+        self.assertEqual(self.map_17.get_maximum_possible_size, 10)
+        self.assertEqual(self.map_18.get_maximum_possible_size, 10)
 
-        self.assertEqual(self.map_19.get_maximum_possible_size, True)
-        self.assertEqual(self.map_20.get_maximum_possible_size, True)
-
+        self.assertEqual(self.map_19.get_maximum_possible_size, 9)
 
